c29dd8e56c9db5ac17503258f3ba303b3be6eb5f
MarawanFadly <marawan.fadly1@gmail.com>
2026-03-25 22:09:36 +0200

    second commit

---
+++ app/app.py
@@ -0,0 +1,20 @@
+import streamlit as st    # streamlit run app/app.py
+from google import genai  # pip install google-genai
+from dotenv import load_dotenv ;     load_dotenv()
+import os
+
+#client = genai.Client( api_key= os.getenv('GEMINI_API_KEY') )
+
+client = genai.Client(api_key="AIzaSyDQMpwtvPZThspLKJ5KOEvC36vtC7avZWk")
+st.header( 'Our First Chatbot' )
+
+user_input = st.text_area( 'Enter your text' )
+
+# create a simple button to handle requests
+if st.button( 'Send' ) :
+    response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents=user_input,
+    )
+
+    st.write( response.text )

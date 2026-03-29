849a10dfa3f020a6d46e01219899c5969b68eaca
MarawanFadly <marawan.fadly1@gmail.com>
2026-03-29 18:20:04 +0200

    change API

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv; load_dotenv()
 
 client = genai.Client(api_key= st.secrets['GEMINI_API_KEY'])
 
-######client = genai.Client(api_key="AIzaSyAISEXbSgqzzQ5RODjHDw1XHZKY8uZMFko")
 st.header( 'Our First Chatbot' )
 
 user_input = st.text_area( 'Enter your text' )
